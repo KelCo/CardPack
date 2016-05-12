c72dc6681c80284608ccd77b1297c0e05b000b89
KelCo <kel7087@morrisbb.net>
2016-05-12 16:14:07 -0400

    Added Project Folder and Basic Classe

---
+++ CardPack/CardPack.java
@@ -0,0 +1,70 @@
+
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+
+public class CardPack extends Application 
+{
+    
+    // State Variables
+    
+    
+    
+    
+    
+    
+    // Constructor
+    
+    public CardPack()
+    {
+        
+        
+        
+        
+        
+        
+    }  
+    
+    // Init
+    public void init()
+    {
+        
+    
+        
+    }
+    
+    // Start
+    public void start(Stage primer)
+    {
+        
+        
+        
+        
+        
+    }
+    
+    
+    // Stop
+    public void stop()
+    {
+        
+        
+        
+        
+    }
+    
+    
+    // Main
+    public static void main(String args[])
+    {
+        
+        
+        
+        
+        
+    }
+    
+}
